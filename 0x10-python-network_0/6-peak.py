db4baac876fff54d373a6324b1301715bfd432b8
glenmiracle18 <glenmiracle67@gmail.com>
2023-05-01 03:13:12 -0700

    python

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,35 +1,31 @@
-def find_peak(list_of_integers):
-    """Find a peak in a list of unsorted integers."""
-    
-    # get the length of the list
-    n = len(list_of_integers)
-    
-    # if the list is empty, return None
-    if n == 0:
-        return None
-    
-    # if the list has only one element, return it
-    if n == 1:
-        return list_of_integers[0]
-    
-    # initialize variables for the start and end of the list
-    start = 0
-    end = n - 1
-    
-    # loop until start and end meet
-    while start < end:
-        
-        # calculate the midpoint of the list
-        mid = (start + end) // 2
-        
-        # if the midpoint is a peak, return it
-        if list_of_integers[mid] > list_of_integers[mid + 1]:
-            end = mid
-        
-        # otherwise, move towards the higher side of the list
-        else:
-            start = mid + 1
-    
-    # return the peak value
-    return list_of_integers[start]
+#!/usr/bin/python3
+""" Finds a peak in a list of unsorted integers
+"""
 
+
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
+        return None
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
