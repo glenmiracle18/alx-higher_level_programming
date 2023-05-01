eebc51ad335342d0690cb66621ebc5d5f9e9cd55
glenmiracle18 <glenmiracle67@gmail.com>
2023-05-01 10:53:21 -0700

    doon

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,22 +1,13 @@
 #!/usr/bin/python3
-
-# Import the 'urllib.request' module
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
 
-# Define the URL to be fetched
-url = 'https://alx-intranet.hbtn.io/status'
 
-# Send a GET request to the URL and store the response in a 'response' object
-with urllib.request.urlopen(url) as response:
-    # Read the content of the response and store it as a bytes object in the 'content' variable
-    content = response.read()
-    
-    # Decode the content from bytes to UTF-8 and store it as a string in the 'utf8_content' variable
-    utf8_content = content.decode('utf-8')
-
-# Print out information about the response
-print("Body response:")
-print(f"\t- type: {type(content)}")
-print(f"\t- content: {content}")
-print(f"\t- utf8 content: {utf8_content}")
-
+if __name__ == "__main__":
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
