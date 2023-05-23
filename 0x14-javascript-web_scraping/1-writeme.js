5b94bbd0f5c3d50d69d95d4a280dc69d445b3b48
glenmiracle18 <glenmiracle67@gmail.com>
2023-05-23 08:25:34 -0700

    done

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filename, 'utf-8', (content, error) => {
+fs.writeFile(filename, content, 'utf-8', (error) => {
 	if (error) {
 		console.log(error);
 	}
