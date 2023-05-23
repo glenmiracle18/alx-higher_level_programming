ba7f28f6d0882bc9683ece2317b996a504070ebf
glenmiracle18 <glenmiracle67@gmail.com>
2023-05-23 08:28:17 -0700

    this is it

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const fileName = process.argv[2];
+const filename = process.argv[2];
 const content = process.argv[3];
 
 fs.writeFile(filename, content, 'utf-8', (error) => {
-	if (error) {
-		console.log(error);
-	}
+  if (error) {
+    console.log(error);
+  }
 });
