1103bd82fb6dccb141d3364f0173472a4876c443
glenmiracle18 <glenmiracle67@gmail.com>
2023-05-23 08:16:09 -0700

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,10 @@
 const fs = require('fs');
 const filename = process.argv[2];
 
-fs.readFile(filename, 'uft-8', (error, content) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log(content);
-	}
+fs.readFile(filename, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
 });
