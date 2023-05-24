f7d547c7dcc4f598f652d21e2cfcace97789b214
glenmiracle18 <glenmiracle67@gmail.com>
2023-05-24 00:41:10 -0700

    done

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movie_id = process.argv[2];
-const url_api = 'https://swapi-api.alx-tools.com/api/films/${movie_id}';
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-
-request.get(url_api, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else {
-        const data = JSON.parse(body);
-        console.log(data.title);
-    }
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+    console.log(data.title);
+  }
 });
