cbb5ce955e8d51b4bfd21551b329efd915e01967
glenmiracle18 <glenmiracle67@gmail.com>
2023-05-24 01:13:58 -0700

    done

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,16 +3,16 @@
 const request = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filepath = process.argv[3];
+const file = process.argv[3];
 
-request.get(url, 'utf-8', (error, reponse, body) => {
-    if (error) {
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
         console.log(error);
-    } else {
-        fs.writeFile(filepath, body, 'utf8', (error) => {
-            if (error) {
-                console.log(error);
-            }
-        })
-    }
-})
+      }
+    });
+  }
+});
